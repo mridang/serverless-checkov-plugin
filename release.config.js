498c50a4e9e143d644a418b0464325ce09c45b93
Mridang Agarwalla <mridang@nosto.com>
2024-06-03 10:48:20 +0530

    Updated the semantic release to also publish to Github Packages

--- release.config.js
+++ release.config.js
@@ -3,7 +3,27 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/github',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true
+      }
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'dist/**', label: 'Distribution files' }
+        ]
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
   ],
+  repositoryUrl: 'https://github.com/mridang/serverless-checkov-plugin'
 };
